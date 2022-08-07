526fbc926597fa2683a8adbad8f129be4e4847aa
otinu <fitshter@gmail.com>
2022-08-07 19:02:04 +0900

    [fix]6

--- js/6.js
+++ js/6.js
@@ -1,11 +1,29 @@
 
 const app = Vue.createApp({
     data: () => ({
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        basePrice: 100
     }),
     computed: {
         reversedMessage: function() {
             return this.message.split('').reverse().join('')
+        },
+        taxIncludedPrice: {
+            get: function() {
+                return this.basePrice * 1.1
+            },
+            // セッターはここでは使われていない
+            set: function(value) {
+                this.basePrice = value / 1.1
+            }
+        },
+        computedNumber: function() {
+            return Math.random()
+        }
+    },
+    methods: {
+        methodsNumber: function() {
+            return Math.random()
         }
     }
 })
