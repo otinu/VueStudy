7ed09d7cfd5b4ffe081e2dd2d566a4f1cd3e180a
otinu <fitshter@gmail.com>
2022-08-11 09:12:41 +0900

    [fix]9

--- js/9.js
+++ js/9.js
@@ -1,50 +1,14 @@
 const app = Vue.createApp({
-    data: () => ({
-      items: null,
-      keyword: '',
-      message: ''
-    }),
-    watch: {
-      keyword: function(newKeyword) {
-        console.log(newKeyword)
-        this.message = 'Waiting for you to stop typing...'
-        this.debouncedGetAnswer()
-      }
-    },
-    mounted: function() {
-      // this.keyword = 'JavaScript'
-      // this.getAnswer()
-
-      // https://lodash.com/docs#debounce
-      // 「_.debounce()」という形式で呼び出す
-      // 第二引数で指定する時間分、処理を遅らせてから第一引数を実行する(1文字打つ毎にAPIへリクエストを送る事態を防ぐ)
-      // 第二引数では、ミリ秒単位で指定
-      this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
-    },
-    methods: {
-      getAnswer: function() {
-        if(this.keyword === '') {
-          console.log('karamoji')
-          this.items = null
-          return
-        }
-  
-        this.message = 'Lading...'
-        const vm = this
-        const params = { page: 1, per_page: 20, query: this.keyword }
-        axios.get('https://qiita.com/api/v2/items', { params })
-             .then(function(response){
-               // console.log(response)
-               vm.items = response.data
-             })
-             // 例外処理のような記述になっている
-             .catch(function(error){
-               vm.message = 'Error!' + error
-             })
-             .finally(function() {
-               vm.message = ''
-             })
-      }
+  data: () => ({
+    isLarge: true,
+    hasError: true,
+    largeClass: 'large',
+    dangerClass: 'text-danger',
+    classObject: {
+      large: true,
+      'text-danger': true
     }
   })
-  app.mount('#app')
+})
+
+app.mount('#app')
